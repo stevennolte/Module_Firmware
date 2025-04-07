fafccf1470b0dbf91cf9088b20fb4d63745490ef
stevennolte <138683349+stevennolte@users.noreply.github.com>
2025-04-07 14:48:48 -0500

    Update WAS.cpp

--- ESP32_AIO/src/WAS.cpp
+++ ESP32_AIO/src/WAS.cpp
@@ -25,8 +25,10 @@ void WAS::loop(){
     } else {
         espConfig->steerCfg.wirelessWAS = false;
     }
-    if (espConfig->progData.adsState == 1 && espConfig->steerCfg.wirelessWAS == false){ 
+    if (espConfig->progData.adsState == 1 && espConfig->steerCfg.wirelessWAS == false && espConfig->steerCfg.useADS == 1){ 
         espConfig->steerData.actSteerAngle = ads->readADC_SingleEnded(0);
+    } else {
+        espConfig->steerData.actSteerAngle = 0.0;
     }
     vTaskDelay(10);
     #else
